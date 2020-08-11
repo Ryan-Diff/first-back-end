eb37c71af2106294bbd9e13ed30fc2cd429e9ddd
ryan diffenbaugh <ryandiffenbaugh@gmail.comgit config --global user. name Ryan-Diffgit config --global user.email ryandiffenbaugh@gmail.com>
2020-08-11 10:50:03 -0700

    initial commit

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
+require('dotenv').config();
 const express = require('express');
 const cors = require ('cors');
 const geoData = require('./data/geo.js');
 const weatherData = require('./data/weather.js');
 const { json } = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 
